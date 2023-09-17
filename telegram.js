a5207611ea92cfcb2628fb15e2638e65fc4a35f9
Sand Despotism <wowow3722123@gmail.com>
2023-09-17 20:57:33 +0300

    please give me more tasks

--- telegram.js
+++ telegram.js
@@ -13,15 +13,14 @@ if (tg.initDataUnsafe.user) {
   profile_img_url = tg.initDataUnsafe.user.photo_url;
 }
 let profile_name = document.querySelector("#profile_avatar > p");
-let profile_img = document.querySelector('#profile_image');
+let profile_img = document.querySelector("#profile_image");
 function setName() {
   if (username) {
     profile_name.innerText = `${first_name} ${last_name}`;
-    // profile_img.src = profile_img_url;
-
+    profile_img.src = profile_img_url;
   } else {
     profile_name.innerText = "Иван Иванов";
-    // profile_img.src = '';
+    profile_img.src = 'profile_test.jpg';
   }
 }
 setName();
@@ -190,23 +189,33 @@ fetch("https://pop.applepodsblack.ru/api/products")
     }
   });
 //разделение по категориям потом зах*ячу
-async function getTrack(trackNum) {
+async function getTrack(trackNum, eventObj) {
+  eventObj.preventDefault();
   const response = await fetch(
     `https://api.track24.ru/tracking.json.php?apiKey=d7bd08383f431cf8913f1eb29585c01a&domain=localhost&pretty=true&code=${trackNum}`
   );
   const track = await response.json();
   console.log(track);
-  let states = document.getElementsByClassName("track_info_state");
-  let header = document.getElementsByClassName("track_info_header");
-  if (track.status == "ok") {
-    header[0].innerText = `Заказ №${trackNum}`;
-    states[0].innerText = `${track.data.destinationPostalAddress}`;
-    states[1].innerText = `${
-      track.data.events[track.data.events.length - 1]
-        .operationAttributeTranslated
-    }`;
+  let dostavka_content = document.querySelector(".dostavka_content");
+  if (track.status == "ok" && document.getElementsByClassName('track_info').length <= 3) {
+    dostavka_content.insertAdjacentHTML(
+      "beforeend",
+      `
+    <div class="track_info">
+                        <p class="track_info_header">Заказ номер ${trackNum}</p>
+                        <p class="track_info_addrstate">Адрес доставки:</p>
+                        <p class="track_info_state">${
+                          track.data.destinationPostalAddress
+                        }</p>
+                        <p class="track_info_addrstate">Текущий статус:</p>
+                        <p class="track_info_state">${
+                          track.data.events[track.data.events.length - 1]
+                            .operationAttributeTranslated
+                        }</p>
+                      </div>`
+    );
   } else {
-    header[0].innerText = "ОШИБКА!";
+    dostavka_content.innerText('ОШИБКА');
   }
 }
 // getTrack('LV668867798CN');
@@ -225,7 +234,7 @@ function changeVarColor(id) {
   let variants = document.getElementsByClassName("variant");
   for (let elem of variants) {
     elem.style.border = "none";
-    elem.style.opacity = '50%';
+    elem.style.opacity = "50%";
   }
   variants[id].style.border = "2px solid var(--Yellow-gradient, #f5ea99)";
   variants[id].style.opacity = "100%";
@@ -242,35 +251,33 @@ function changeVarColor(id) {
     .owlCarousel("update");
 }
 //функция для кастомного dropdawn
-function dropdawn(button_div, content_div){
+function dropdawn(button_div, content_div) {
   let content = document.querySelector(`#${content_div}`);
-  let button =  document.querySelector(`#${button_div}`);
+  let button = document.querySelector(`#${button_div}`);
   let styles = getComputedStyle(content);
-  if (styles.display == 'flex'){
-    content.style.display = 'none';
-    button.src = 'arrow_down.svg';
+  if (styles.display == "flex") {
+    content.style.display = "none";
+    button.src = "arrow_down.svg";
   } else {
-    content.style.display = 'flex';
-    button.src = 'arrow_up.svg';
-
+    content.style.display = "flex";
+    button.src = "arrow_up.svg";
   }
-  
 }
 //функция смены валюты
-function changeCurrency(event){
-  let currency_div = document.querySelector('#main_info_currencylogo')
-  if (event.target.value == 'rub'){
-    currency_div.innerText = '₽ RUB';
-  } else if (event.target.value == 'eur'){
-    currency_div.innerText = '€ EUR';
-  } else if (event.target.value == 'byn'){
-    currency_div.innerText = 'Br BYN';
+function changeCurrency(event) {
+  let currency_div = document.querySelector("#main_info_currencylogo");
+  if (event.target.value == "rub") {
+    currency_div.innerText = "₽ RUB";
+  } else if (event.target.value == "eur") {
+    currency_div.innerText = "€ EUR";
+  } else if (event.target.value == "byn") {
+    currency_div.innerText = "Br BYN";
   }
 }
 //функция для загрущки и отображения данных товара на отдельной странице
-async function productPage(){
-  let response = await fetch('https://pop.applepodsblack.ru/api/products?populate=deep');
+async function productPage() {
+  let response = await fetch(
+    "https://pop.applepodsblack.ru/api/products?populate=deep"
+  );
   let data = await response.json();
-
-
 }
